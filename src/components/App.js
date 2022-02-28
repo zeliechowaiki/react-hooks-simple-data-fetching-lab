f1973f4692ff9ab13ee312d144467feb1f25a35e
Zelie Chowaiki <zeliechowaiki@gmail.com>
2022-02-28 11:53:30 -0500

    commit tree a8f38529af06c0020fd162079e2eb40043bfee36

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,18 @@
 // create your App component here
+import {React, useState, useEffect} from "react";
+
+function App() {
+    const [dogs, setDogs] = useState([]);
+
+    useEffect(() => {
+        fetch('https://dog.ceo/api/breeds/image/random')
+        .then(resp => resp.json())
+        .then(dog => setDogs(dog.message));
+    },[]);
+
+    if (dogs.length === 0) return <p>Loading...</p>
+
+    return <img src={dogs} alt="A Random Dog" />
+}
+
+export default App;
